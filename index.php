a74121174afc2ab9ae71436522fff9cb0cd1498b
chaosannals <chaosannals@outlook.com>
2019-01-28 00:45:44 +0800

    修改目录结构。

--- index.php
+++ index.php
@@ -12,17 +12,20 @@
 // 应用入口文件
 
 // 检测PHP环境
-if (version_compare(PHP_VERSION, '5.3.0', '<')) {
-    die('require PHP > 5.3.0 !');
+if (version_compare(PHP_VERSION, '5.5.0', '<')) {
+    die('require PHP > 5.5.0 !');
 }
+
+// 加载 Composer 自动加载。
+require __DIR__.'/Vendor/autoload.php';
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG', true);
 
 // 定义应用目录
-define('APP_PATH', dirname(__DIR__).'/');
+define('APP_PATH', __DIR__.'/Application/');
 
 // 引入ThinkPHP入口文件
-require APP_PATH.'Thr33/ThinkPHP.php';
+require __DIR__.'/ThinkPHP/Thr33.php';
 
 // 亲^_^ 后面不需要任何代码了 就是如此简单
